1f230b2dd7e2a196ccd965f94aee67f1a7534ad0
fac3lessd0ge <garmasharseniy@gmail.com>
2022-08-22 06:10:13 +0300

    added _auth token in request headers

--- src/redux/api/api.ts
+++ src/redux/api/api.ts
@@ -1,7 +1,22 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery, retry } from '@reduxjs/toolkit/query/react';
+import { RootState } from '../store';
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: process.env.REACT_APP_BASE_URL,
+  prepareHeaders: (headers, { getState }) => {
+    // By default, if we have a token in the store, let's use that for authenticated requests
+    const token = (getState() as RootState).userSlice._auth
+    if (token) {
+      headers.set('Authentication', `Bearer ${token}`)
+    }
+    return headers
+  },
+})
+
+const baseQueryWithRetry = retry(baseQuery, { maxRetries: 6 })
 
 export const api = createApi({
   reducerPath: 'baseapi',
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
+  baseQuery: baseQueryWithRetry,
   endpoints: () => ({})
 })
--- src/redux/slices/userSlice.ts
+++ src/redux/slices/userSlice.ts
@@ -1,5 +1,4 @@
 import { createSlice } from "@reduxjs/toolkit";
-import type { PayloadAction } from '@reduxjs/toolkit';
 
 interface IUserState {
   _auth: string
